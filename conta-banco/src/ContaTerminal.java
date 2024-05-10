5eac9444f69881a6cf673f40c7d625418e394b8e
Jayme Alves <jaymealves123@hotmail.com>
2024-05-10 19:08:56 -0300

    ContaTerminalDesafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO: conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da Conta:");
+        int numConta = scanner.nextInt();
+
+        System.out.println("Digite o numero da agencia:");
+        String numAgencia = scanner.next();
+
+        System.out.println("Digite o Saldo em conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Digite o nome do Cliente:");
+        String nomeCliente = scanner.next();
+
+        
+        System.out.println("Numero da Conta é: " + numConta);
+        System.out.println("Numero da agencia é: " + numAgencia);
+        System.out.println("Nome do Cliente é: " + nomeCliente);
+        System.out.println("Saldo em conta è: " + saldo);
+       
+
+    
+
+
+         
+        
+        // Exibir as mensagem para o nosso usuario
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada 
     }
 }
